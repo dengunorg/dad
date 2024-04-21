3af35587734f00cc12a931fdd9877dbad9f45ae5
Miguel Costa <Painatalman@users.noreply.github.com>
2024-04-21 13:07:56 +0100

    Fix: adjust base URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://dad.dengun.net",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/dad/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
